59f73ae1f94b268fe28f535a9d2cd041ce214982
rrojasn17 <53195263+rrojasn17@users.noreply.github.com>
2021-06-18 10:11:42 -0600

    commit

--- include/user-variables.h
+++ include/user-variables.h
@@ -3,10 +3,12 @@
 //CONEXION CON IoTPROJECTS
 String dId = " ";
 String webhook_pass = " ";
-String webhook_endpoint = "http://<iotprojects_here>:3001/api/getdevicecredentials";
-const char *mqtt_server= "<iotprojects_here>";
+String webhook_endpoint = "http://3.142.89.107:3001/api/getdevicecredentials";
+const char *mqtt_server= "app.iotcostarica.ml";
 
 
 //CONFIGURACION DE WiFi
-const char *wifi_ssid = "";
-const char *wifi_password = "";
+const char *wifi_ssid = "IoTCR_Wifi";
+const char *wifi_password = "- - - -";
+
+const long sendDBInterval = 300000;
